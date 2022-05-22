1fce47aa32687575f181dbb7c0dc6f2fb7410e04
unknown <arfinchowdhuryarif7@gmail.com>
2022-05-22 09:20:35 +0600

    done

---
+++ pages/NavBar/Navbar.js
@@ -0,0 +1,104 @@
+import React from "react";
+import styles from "./NavBar.module.css";
+const Navbar = () => {
+  return (
+    <div className={styles.NavbarBackground}>
+      <nav className="navbar navbar-expand-lg navbar-light ">
+        <div className="container-fluid">
+          <a
+            className="navbar-brand NavName"
+            href="/"
+            style={{ fontSize: 30, fontWeight: 800 }}
+          >
+            Essa World
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Glocery
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Bakery and Bread
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Pasta and Rice
+                    </a>
+                  </li>
+
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Soups and Canned Goods.
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+              <input
+                className="form-control mx-2"
+                type="search"
+                placeholder="🔍 Search Your Product from here"
+                
+              />
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Glocery
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Bakery and Bread
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Pasta and Rice
+                    </a>
+                  </li>
+
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Soups and Canned Goods.
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
